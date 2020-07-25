aeccc3553752f0d638f1a21583979ebc3dc3da3e
Erik <3salaz.dev@gmail.com>
2020-07-25 15:03:10 -0700

    added script for game manager and added to prefab

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance = null;
+
+    public enum GameState
+    {
+        INTRO,
+        BERKLAND,
+        BATTLE,
+        RESULTS,
+        ENDING
+    }
+    private GameState currentState = GameState.INTRO;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+        set { }
+    }
+
+    // Global Variables?!
+    private int howManyBattleWon = 0;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("[GameManager] There are multiple instances of GameManager.");
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(this);
+    }
+
+    void Update()
+    {
+        switch (currentState)
+        {
+            case GameState.INTRO:
+                break;
+
+            case GameState.BERKLAND:
+                break;
+
+            case GameState.BATTLE:
+                break;
+        }
+
+    }
+
+    public void SetState(GameState newState)
+    {
+        Debug.Log("[GameManager] ENTERING SCENE " + newState);
+        currentState = newState;
+
+        // which state we are going to
+        switch (currentState)
+        {
+            case GameState.INTRO:
+                SceneManager.LoadScene("Main");
+                break;
+
+            case GameState.BERKLAND:
+                SceneManager.LoadScene("Berkland");
+                break;
+
+            case GameState.BATTLE:
+                SceneManager.LoadScene("Battle");
+                break;
+        }
+    }
+}
