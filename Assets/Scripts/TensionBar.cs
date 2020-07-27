d8406fa888547160e502f687cba9b23bc2cd12be
Erik <3salaz.dev@gmail.com>
2020-07-27 15:11:33 -0700

    ui tention bar and script

---
+++ Assets/Scripts/TensionBar.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TensionBar : MonoBehaviour
+{ // Creating a public instance of Slider from Unity UI
+    public Slider slider;
+    public Gradient gradient;
+    public Image fill; 
+    public void SetTention(int tention)
+    {
+        slider.value =  tention;
+
+    }
+
+    public void SetMaxTention(int tention)
+    {
+        slider.maxValue = tention;
+        slider.value = tention;
+
+        fill.color = gradient.Evaluate(1f);
+    }
+
+    public void SetHealth(int tention)
+    {
+        slider.value = tention;
+
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    }
+}
