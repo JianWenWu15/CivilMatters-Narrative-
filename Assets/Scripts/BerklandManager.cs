50144a6e4961f3430741cf6828e033de142d125d
Erik <3salaz.dev@gmail.com>
2020-07-27 15:26:07 -0700

    pulled demo

--- Assets/Scripts/BerklandManager.cs
+++ Assets/Scripts/BerklandManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BattleLogic : MonoBehaviour
+public class BerklandManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
