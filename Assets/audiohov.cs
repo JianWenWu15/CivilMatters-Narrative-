22cf747f962b98e54be22e9130feff7fe0eb1115
xenoslayerlv426 <jondmjr@gmail.com>
2020-07-31 11:44:28 -0700

    Main Menu changes

---
+++ Assets/audiohov.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audiohov : MonoBehaviour
+{
+    public AudioSource myFx;
+    public AudioClip hoverFx;
+
+
+
+    public void HoverSound()
+    {
+        myFx.PlayOneShot(hoverFx);
+    }
+}
