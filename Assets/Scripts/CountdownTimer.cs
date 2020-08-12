ce7b1a397266b8aa7dee1873e908bda062b69623
Erik <3salaz.dev@gmail.com>
2020-08-12 11:33:42 -0700

    cleaned up location of folders

--- Assets/Scripts/CountdownTimer.cs
+++ Assets/Scripts/CountdownTimer.cs
@@ -10,12 +10,12 @@ public class CountdownTimer : MonoBehaviour
     float startingTime = 10f;
     [SerializeField] Text countdownText;
 
-    public override bool Equals(object obj)
-    {
-        return obj is CountdownTimer timer &&
-               base.Equals(obj) &&
-               EqualityComparer<Text>.Default.Equals(countdownText, timer.countdownText);
-    }
+    // public override bool Equals(object obj)
+    // {
+    //     return obj is CountdownTimer timer &&
+    //            base.Equals(obj) &&
+    //            EqualityComparer<Text>.Default.Equals(countdownText, timer.countdownText);
+    // }
 
     void Start()
     {
---
+++ Assets/Scripts/DialogueUi.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// using PixelCrushers.DialogueSystem;
+
+public class DialogueUi : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        // FadeInScene
+
+        // Charactors---
+        // Tention Bar--
+        // Narrator Panel--
+
+        // Set Tention
+        // 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
