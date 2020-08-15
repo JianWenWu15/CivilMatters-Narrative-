c8400ca909c781222809cb6217693c3a6eeb3541
Erik <3salaz.dev@gmail.com>
2020-08-15 12:47:01 -0700

    battle scene art

---
+++ Assets/BattleManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // change canvas background  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
